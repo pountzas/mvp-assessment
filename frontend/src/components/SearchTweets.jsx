eaaf45dcfbc845f2857e47453d045c4678af033a
pountzas <nikos@pountzas.gr>
2023-01-24 21:32:57 +0200

    create logic for searchTweet

--- frontend/src/components/SearchTweets.jsx
+++ frontend/src/components/SearchTweets.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { Autocomplete, createFilterOptions } from "@mui/material";
+import TextField from "@mui/material/TextField";
 
-function SearchTweets() {
-  return <div>SearchTweets</div>;
-}
+import { useRecoilState } from "recoil";
+import { tweetListAtom } from "../atoms/tweetsAtom";
+
+const SearchTweets = () => {
+  const [search] = useState("");
+  const [selectedTweet, setSelectedTweet] = useState(null);
+  const [setTweets] = useState([]);
+
+  const [tweetsStateGlobal] = useRecoilState(tweetListAtom);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const filteredTweets = tweetsStateGlobal.filter((tweet) =>
+      tweet.text.includes(search.text)
+    );
+    setTweets(filteredTweets);
+  };
+
+  return (
+    <>
+      {" "}
+      <form onSubmit={handleSearch}>
+        <Autocomplete
+          id="free-solo-demo"
+          freeSolo
+          className=" bg-gray-200 outline-none"
+          filterOptions={createFilterOptions({ limit: 5 })}
+          options={tweetsStateGlobal}
+          getOptionLabel={(tweet) => (tweet.text ? tweet.text : "")}
+          onChange={(e, tweet) => setSelectedTweet(tweet)}
+          sx={{
+            outline: "none",
+            strokeWidth: "0",
+            MuiOutlinedInput: { border: "none", strokeWidth: "0" }
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Search tweets"
+              variant="outlined"
+              fullWidth
+            />
+          )}
+        />
+      </form>
+      <div className="flex flex-col justify-start bg-gray-200 mt-[25%] min-h-[40%]">
+        {selectedTweet && (
+          <div className=" p-4">
+            <p className="font-bold text-lg">{selectedTweet.text}</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default SearchTweets;
