72ec6c383717b5330238a862c3231cab734f9808
pountzas <nikos@pountzas.gr>
2023-01-24 19:09:43 +0200

    add tweets, TweetCreator components functionality

--- frontend/src/components/TweetCreator.jsx
+++ frontend/src/components/TweetCreator.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import axios from "axios";
+import { useRecoilState } from "recoil";
+import { tweetListAtom } from "../atoms/tweetsAtom";
+
 function TweetCreator() {
-  return <div>TweetCreator</div>;
+  const [text, setText] = useState("");
+  const [tweetsStateGlobal, setTweetsStateGlobal] =
+    useRecoilState(tweetListAtom);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:5000/api/posts", { text }).then((res) => {
+      console.log(res.data);
+      setTweetsStateGlobal([{ text }, ...tweetsStateGlobal]);
+    });
+    setText("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="What's happening?"
+      />
+      <button type="submit">Tweet</button>
+    </form>
+  );
 }
 
 export default TweetCreator;
--- frontend/src/components/Tweets.jsx
+++ frontend/src/components/Tweets.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import _ from "lodash";
+
+import { useRecoilState } from "recoil";
+import { tweetListAtom } from "../atoms/tweetsAtom";
 
 function Tweets() {
-  return <div>Tweets</div>;
+  const [tweets, setTweets] = useState([]);
+  const [tweetsStateGlobal, setTweetsStateGlobal] =
+    useRecoilState(tweetListAtom);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/posts").then((res) => {
+      const sortedTweets = _.sortBy(res.data, "createdAt").reverse();
+
+      setTweets(sortedTweets);
+      setTweetsStateGlobal(sortedTweets);
+    });
+    console.log("Tweets useEffect", tweets);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    tweets !== tweetsStateGlobal && setTweets(tweetsStateGlobal);
+    console.log("Tweets useEffect", tweets);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [tweetsStateGlobal]);
+
+  return (
+    <ul>
+      {tweets?.map((tweet) => (
+        <li key={tweet._id}>
+          <p>{tweet.text}</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tweets;
